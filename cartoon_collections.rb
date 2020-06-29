76c7ea9e65d4d97e6be502ca16f10f058773c778
A D <amitdesh2014@gmail.com>
2020-06-29 17:28:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,5 +22,5 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.detect do |elem|
     valid_calls.any? do |ee|
-    
+  end
 end
